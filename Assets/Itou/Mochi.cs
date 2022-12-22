e3a2f3ef076e54217f00ae9ea12cae79766b2bd1
伊藤廣軒 <106290558+ItouHiroken@users.noreply.github.com>
2022-12-22 12:17:44 +0900

    [Update]おもちが左右移動するようになった、止まるようにもなったよ

--- Assets/Itou/Mochi.cs
+++ Assets/Itou/Mochi.cs
@@ -4,32 +4,56 @@ using UnityEngine;
 
 public class Mochi : MonoBehaviour
 {
+    [SerializeField] string _p1ButtonName = "Submit";
+    [SerializeField] string _p2ButtonName = "Jump";
     [SerializeField] Transform _startMarker;
     [SerializeField] Transform _endMarker;
-    [SerializeField] float speed = 1.0F;
-    [SerializeField] bool _otosu;
+    [SerializeField] float _speed = 1.0f;
+    [SerializeField] bool _otosu = false;
     [SerializeField] float _time;
-    Transform _middleTransform;
-
+    private float distance_two;
     bool _player = false;
-    /// <summary>
-    /// Trueの時P1,Falseの時P2
-    /// </summary>
+    /// <summary>Trueの時P1,Falseの時P2</summary>
     public bool Player { get => _player; set => _player = value; }
     void Start()
     {
-
+        //二点間の距離を代入
+        distance_two = Vector3.Distance(_startMarker.position, _endMarker.position) / 2;
+        gameObject.transform.position = (_startMarker.position + _endMarker.position) / 2;
     }
     void Update()
     {
+        _time += Time.deltaTime * _speed;
         if (!_otosu)
         {
             LRMove();
         }
+        PlayerChange();
     }
+    /// <summary>プレイヤーの切り替えで入力受け取りが違うようにした場所 </summary>
+    void PlayerChange()
+    {
+        if (Player)
+        {
+            if (Input.GetButtonDown(_p1ButtonName))
+            {
+                _otosu = true;
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown(_p2ButtonName))
+            {
+                _otosu = true;
+            }
+        }
+    }
+    /// <summary>
+    /// 左右移動する君
+    /// </summary>
     void LRMove()
     {
-        _time += Time.deltaTime;
-        Mathf.Sin(_time);
+        float sin = Mathf.Sin(_time);
+        this.transform.position = new Vector3(sin * distance_two, 0);
     }
 }
