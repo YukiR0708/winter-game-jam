f424d2e65e3480972489aa218157c539a78e1425
CHARA\meich <mei.charaa@gmail.com>
2022-12-22 12:35:53 +0900

    [update]みかん落ちてきて、クリア・失敗が出てくる処理を書いた(未テスト)

--- Assets/Tamura/ResultManager.cs
+++ Assets/Tamura/ResultManager.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ResultManager : MonoBehaviour
 {
     GameManager _gameManager;
+    StackMochiManager _stackMochiManager;
     bool _isShownResult = false;
+    [SerializeField, Header("みかんをどのくらい上に出現させるか")] float _orangeRange = 10;
     [SerializeField, Header("みかん")] GameObject _orange;
+    [SerializeField, Header("クリア時の画面")] GameObject _clear;
+    [SerializeField, Header("失敗時の画面")] GameObject _failed;
 
     private void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
+        _stackMochiManager = FindObjectOfType<StackMochiManager>();
     }
 
     void Update()
@@ -21,12 +27,12 @@ public class ResultManager : MonoBehaviour
 
             if (_gameManager.state.HasFlag(GameManager.GameStatus.Clear))
             {
-                OrangeFall();
+                OrangeFall(_clear);
                 //クリアしたときのやつ
             }
             else if (_gameManager.state.HasFlag(GameManager.GameStatus.Failed))
             {
-                OrangeFall();
+                OrangeFall(_failed);
                 //失敗したときのやつ
             }
 
@@ -35,20 +41,12 @@ public class ResultManager : MonoBehaviour
     }
 
     /// <summary>みかんが上から降ってくる</summary>
-    private void OrangeFall()
-    {
-        //一番上に積んであるもちを取ってきて、それの上に出現させる
-        //その後、そのもちの上にゆっくりとみかんが落ちていく
-        _isShownResult = true;
-    }
-
-    /// <summary>みかんが上から降ってくる</summary>
     private IEnumerator OrangeFall(GameObject result)
     {
         //一番上に積んであるもちを取ってきて、それの上に出現させる
+        GameObject orange = Instantiate(_orange, new Vector2(_stackMochiManager.LastStackMochi.transform.position.x, _stackMochiManager.LastStackMochi.transform.position.y + _orangeRange), Quaternion.identity);
         //その後、そのもちの上にゆっくりとみかんが落ちていく
-        //↓落ちていくまで待つ
-        yield return new WaitForSeconds(1.0f);
+        yield return orange.transform.DOMoveY(-_orangeRange, 5).SetAutoKill().WaitForCompletion();
         result.SetActive(true); //ここもアニメーションさせたい
     }
 }
