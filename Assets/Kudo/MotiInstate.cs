03f39bd67629ed3ee243de2f194ce698d77a7afd
Otsu01 <kudo.nonoka02@gmail.com>
2022-12-22 13:41:56 +0900

    returnにした

--- Assets/Kudo/MotiInstate.cs
+++ Assets/Kudo/MotiInstate.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class MotiInstate : MonoBehaviour
 {
-    [SerializeField] int motiNum;
-    [SerializeField] GameObject[] moti;
+    [SerializeField, Header("出す餅の合計数")] int motiNum;
+    GameObject[] moti;
     List<GameObject> motis = new List<GameObject>();
-    [SerializeField] Transform OnePpos;
-    [SerializeField] Transform TwoPpos;
+    [SerializeField, Header("1Pの餅の出現場所・協力のときはこの出現場所だけ使う")] Transform OnePpos;
+    [SerializeField, Header("2Pの餅の出現場所")] Transform TwoPpos;
     int count = 0;
     int count2 = 0;
-    
     GameManager gameManager;
-    Mochi mochi;
     public bool isMochiIn = false;
-    public bool isMochiIn2 = false;
+    
 
    
     // Start is called before the first frame update
     void Start()
     {
         isMochiIn = false;
-        isMochiIn2 = false;
-        
         gameManager = FindObjectOfType<GameManager>();
         //FindObjectOfType<>
         //リストに前もって出す餅を全て入れておく
@@ -45,19 +41,22 @@ public class MotiInstate : MonoBehaviour
         }
     }
 
-    public void OneP()
+    public GameObject OneP()
     {
-       GameObject go = Instantiate(moti[count], OnePpos.position, Quaternion.identity);
+        GameObject go = Instantiate(moti[count], OnePpos.position, Quaternion.identity);
         isMochiIn = true;
         count++;
         Mochi mochi = go.GetComponent<Mochi>();
         mochi.Player = !mochi.Player;
+        return go;
     }
 
-    public void SecoundP()
+    public GameObject SecoundP()
     {
-        Instantiate(moti[count2], TwoPpos.position, Quaternion.identity);
-        isMochiIn2 = true;
+        GameObject go = Instantiate(moti[count2], TwoPpos.position, Quaternion.identity);
+        
+        
         count2++;
+        return go;
     }
 }
