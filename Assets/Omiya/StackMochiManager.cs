ae9d879a2b5af22913afbd5088690ab093ff6615
ryuoomiya <oomiyaryu@gmail.com>
2022-12-22 14:44:26 +0900

    [Update] カメラ追尾機能追加

--- Assets/Omiya/StackMochiManager.cs
+++ Assets/Omiya/StackMochiManager.cs
@@ -5,29 +5,20 @@ using UniRx;
 
 public class StackMochiManager : MonoBehaviour
 {
-
     //シングルトン
     static StackMochiManager _instance;
 
     public static StackMochiManager Instance { get { return _instance; } }
 
-    [SerializeField, Header("積んだ餠たち")]
-    List<GameObject> _stackedMochiList = new List<GameObject>();
-
-    [Tooltip("最後に積まれた餠")]
-    private GameObject _lastStackMochi;
-    public GameObject LastStackMochi { get { return _lastStackMochi; } }
-
-    [Tooltip("積まれた餠の数")]
-    IntReactiveProperty _stackMochiCount;
-    //MochiInstateスクリプト
-    [SerializeField] MochiInstate _mochiInstate;
-    //Mochiスクリプト
-
     GameObject _targetMochi;
     Rigidbody2D _targetMochiRb;
     Mochi _targetMochiMochi;
-
+    [SerializeField] GameObject _mainCamera;
+    [SerializeField, Tooltip("MochiInstateスクリプト")] MochiInstate _mochiInstate;
+    [SerializeField, Header("積んだ餠たち")] List<GameObject> _stackedMochiList = new List<GameObject>();
+    [Tooltip("最後に積まれた餠")] private GameObject _lastStackMochi;
+    [Tooltip("積まれた餠の数")] IntReactiveProperty _stackMochiCount;
+    public GameObject LastStackMochi { get { return _lastStackMochi; } }
 
     void Awake()
     {
@@ -55,7 +46,9 @@ public class StackMochiManager : MonoBehaviour
         if (_targetMochiRb.velocity.magnitude == 0 && _targetMochiMochi.Otosu)
         {
             Debug.Log("置けた");
+            //リストに追加
             _stackedMochiList.Add(_targetMochi);
+            //ターゲット変更
             _lastStackMochi = _targetMochi;
             _mochiInstate.IsMochiIn = false;
             _targetMochi = _mochiInstate.OneP();
@@ -64,14 +57,14 @@ public class StackMochiManager : MonoBehaviour
         }
     }
 
-    ///// <summary>一番上の餠を変数に入れる</summary>
-    //void UpdateLastMochi()
-    //{
-    //    //_stackmochiCountの値が変化したら呼ばれる
-    //    _stackMochiCount.Subscribe(m =>
-    //    {
-    //        //_lastStackMochiをUpdateするa
-    //        _lastStackMochi = _stackedMochiList[m - 1];
-    //    }).AddTo(this);
-    //}
+    /// <summary>一番上の餠を変数に入れる</summary>
+    void UpdateLastMochi()
+    {
+        //_stackmochiCountの値が変化したら呼ばれる
+        _stackMochiCount.Subscribe(m =>
+        {
+            //_lastStackMochiをUpdateするa
+            _mainCamera.transform.position = new Vector2(_mainCamera.transform.position.x , _stackedMochiList[m - 1].transform.position.y);
+        }).AddTo(this);
+    }
 }
